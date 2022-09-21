196adcb763614b4867eec40831c047f5b0678764
mananuf <bmananaf@gmail.com>
2022-09-21 22:04:05 +0100

    adds contact method

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,29 +7,40 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     //------ home page -------------
-    public function index(){
+    public function index()
+    {
         $title = 'Welcome To Home Page';    // variable to be passed into blade template
 
         // return view('pages/index', compact('title'));        // way of injectin into template 
-        return view('pages/index') -> with('title', $title);    // preferred way
+        return view('pages/index')->with('title', $title);    // preferred way
     }
 
 
     // ----- about page ------------
-    public function about(){
+    public function about()
+    {
         $title = 'About Us';
 
-        return view('pages/about') -> with('title', $title);
+        return view('pages/about')->with('title', $title);
     }
 
-    
-    // ----- about page ------------
-    public function services(){
+
+    // ----- services page ------------
+    public function services()
+    {
         $data = [
             'title' => 'Our Services',
             'services' => ['Web Developement', 'Machine Learning', 'AI', 'Robotics']
         ];
-        
-        return view('pages/services') -> with($data);   // injecting into blade, with multiple values
+
+        return view('pages/services')->with($data);   // injecting into blade, with multiple values
+    }
+
+    // ----- about page ------------
+    public function contact()
+    {
+        $title = 'Contact us';
+
+        return view('pages/contact')->with('title', $title);
     }
 }
